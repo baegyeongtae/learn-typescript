6fc1b740c4590c83db88b43c8047fc1fb5e63c1d
baegyeongtae <bkt584@gmail.com>
2022-04-29 13:55:12 +0900

    js를 ts처럼 쓰는 방법

--- why-ts/sample.js
+++ why-ts/sample.js
@@ -1,6 +1,18 @@
+// function sum(a, b) {
+//   return a + b;
+// }
+//
+// // sum(10, 20)
+// result.toLocaleString();
+
+// @ts-check
+
+/**
+ * @param {number} a // 첫번째 숫자
+ * @param {number} b // 두번째 숫자
+ */
+
 function sum(a, b) {
   return a + b;
 }
-
-// sum(10, 20)
-result.toLocaleString();
+sum(10, "20"); // ts-check를 import함으로 js 에서 ts의 오류 검출 방식을 알아내볼 수 있다
--- why-ts/sample.ts
+++ why-ts/sample.ts
@@ -4,3 +4,6 @@ function add(a: number, b: number): number {
 
 let result = add(10, 20);
 result.toLocaleString(); // 코드의 추론기능
+
+
+add(10, '20'); //기본적으로 ts는 문법에 맞지 않는 문구의 오류를 검출해준다.
