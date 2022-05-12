566aae3070a830eca70add0f50d57d862c7a8fc7
baegyeongtae <bkt584@gmail.com>
2022-05-13 00:26:03 +0900

    유니온 타입과 인터섹션 타입의 차이점

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -31,12 +31,20 @@ interface Person {
     age: number;
 }
 
+
 // Union
-function askSomeone(someone: Developer | Person): void {
-    someone.name    // Union type을 설정했지만, safe하게 하기 위해서 (보장된) 공통된 타입으로만 지정이 가능하다.
-    someone.age     // 그냥 쓰기엔 unsafe
-    someone.skill   // 그래서 앞서 썼던 typeof ~ 처럼 guard를 사용하여 다시 사용할 수 있다.
-}
+// function askSomeone(someone: Developer | Person) {
+//     // someone.name    // Union type을 설정했지만, safe하게 하기 위해서 (보장된) 공통된 타입으로만 지정이 가능하다.
+//     // someone.age     // 그냥 쓰기엔 unsafe
+//     // someone.skill   // 그래서 앞서 썼던 typeof ~ 처럼 guard를 사용하여 다시 사용할 수 있다.
+// }
+
+// Union이면 두 타입 중 하나만 넘겨도 가능.
+// askSomeone({ name: '디벨로퍼', skill: '웹 개발'});
+// askSomeone({ name: '캡틴', age: 100});
+
+
+
 
 // intersection
 function askSomeone(someone: Developer & Person) {
@@ -45,5 +53,9 @@ function askSomeone(someone: Developer & Person) {
     someone.skill
 }
 
-let seho: string | number | boolean;
-let capt: string & number & boolean;
+// intersection이면 하나의 새로운 타입이 만들어진다고 생각하면 편함!!! 합쳐진 모든 속성을 다 넘겨줘야함.
+askSomeone({ name: '디벨로퍼', skill: '웹 개발', age: 100});
+// askSomeone({ name: '캡틴', age: 100});
+
+// let seho: string | number | boolean;
+// let capt: string & number & boolean;
