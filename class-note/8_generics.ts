3e597cb1913881a91cb2e76ef3f4851c25c4a962
baegyeongtae <bkt584@gmail.com>
2022-05-17 16:29:11 +0900

    제네릭의 기본 문법

---
+++ class-note/8_generics.ts
@@ -0,0 +1,15 @@
+// function logText(text) {
+//     console.log(text);
+//     return text;
+// }
+// logText(10);
+// logText('하이');
+// logText(true);
+
+
+function logText<T>(text: T): T {
+    console.log(text);
+    return text;
+}
+logText('하이');
+logText<string>('하이');
