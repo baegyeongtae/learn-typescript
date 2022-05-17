d13b026711fa619c3fd09d0014b1a26ea269ac04
baegyeongtae <bkt584@gmail.com>
2022-05-17 16:17:27 +0900

    자바스크립트 프로토타입의 활용 사례

--- class-note/prototype.js
+++ class-note/prototype.js
@@ -9,3 +9,18 @@ console.log(admin);
 
 admin.role = 'admin';
 console.log(admin);
+
+/////////////////////////////////////////////////////////////////////////////
+
+let obj = { a: 10 };        // undefined
+
+Object.keys(obj);           // ["a"]
+obj.hasOwnProperty('a');    // true
+
+console.log(obj);
+
+// obj 는 상위 __proto__가 Object이기 때문에, obj. 했을때 쓸 수 있는 메서드를 사용할 수 있음.
+
+let arr = [];
+
+// arr 는 상위 __proto__가 Array이기 때문에, arr. 했을때 쓸 수 있는 메서드를 사용할 수 있음.
